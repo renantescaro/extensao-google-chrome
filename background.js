f9fff80a6caf2f449c9299ebc1f310397df010d7
renantescaro <renantescaro@yahoo.com.br>
2019-10-23 21:05:20 -0300

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,93 @@
 
+consultaApi();
+
 this.document.getElementById('btnSalvar').addEventListener("click", salvar);
 this.document.getElementById('btnAdicionar').addEventListener("click", abrirCadastro);
 this.document.getElementById('btnCancelar').addEventListener("click", fecharCadastro);
-this.document.getElementById('btnCancelar').addEventListener("click", editarcadastro);
+
+function consultaApi(){
+    
+    var get = new XMLHttpRequest();
+
+    get.open("GET", "", true);
+
+    get.send();
+
+    get.onreadystatechange = function(){
+    
+        if (get.readyState == 4 && get.status == 200) {
+
+            montarListagem(get.responseText);
+        }
+    }
+}
+
+function enviarDadosApi(){
+
+    var titulo = document.getElementById('txtTitulo').value;
+    var descricao = document.getElementById('txtDescricao').value;
+
+    var post = new XMLHttpRequest();
+
+    post.open("POST", "", true);
+
+    post.send();
+
+    post.onreadystatechange = function(){
+    
+        if (post.readyState == 4 && post.status == 200) {
+
+            
+        }
+    }
+}
+
+function montarListagem(json){
+
+    /*
+        [
+            {"anotacao":{"titulo":"almoco","descricao":"almocar com meus pais"}},
+            {"anotacao":{"titulo":"Noite","descricao":"Passear com o cachorro"}}
+        ]
+    */
+
+    var obj = JSON.parse(json);
+
+    obj.forEach(a => {
+
+        var tr = document.createElement('tr');
+        var tdId = document.createElement('td')
+        var tdTitulo = document.createElement('td');
+        var tdBtnEditar = document.createElement('td');
+        var btnEditar = document.createElement('button');
+
+        tdId.style.display = 'none';
+        btnEditar.className = 'btn btn-primary editar';
+        btnEditar.textContent = 'Editar';
+        btnEditar.onclick = editarCadastro;
+
+        tdId.textContent = a.anotacao.id;
+        tdTitulo.textContent = a.anotacao.titulo;
+
+        tdBtnEditar.appendChild(btnEditar);
+        tr.appendChild(tdId);
+        tr.appendChild(tdTitulo);
+        tr.appendChild(tdBtnEditar);
+
+        document.getElementById('tbodyListagem').appendChild(tr);
+    });
+}
 
 function salvar(){
 
-    document.getElementById('txtTitulo').value = '';
-    document.getElementById('txtConteudo').value = '';
-
+    enviarDadosApi();
+    
     document.getElementById('listagem').style.display = '';
     document.getElementById('cadastro').style.display = 'none';
     document.getElementById('btnExcluir').style.display = 'none';
+    
+    document.getElementById('txtTitulo').value = '';
+    document.getElementById('txtConteudo').value = '';
 }
 
 function abrirCadastro(){
@@ -20,7 +96,7 @@ function abrirCadastro(){
     document.getElementById('cadastro').style.display = '';
 }
 
-function editarcadastro(){
+function editarCadastro(){
 
     document.getElementById('listagem').style.display = 'none';
     document.getElementById('cadastro').style.display = '';
